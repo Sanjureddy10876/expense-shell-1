6d55839839033120bb177d603b644c005f09095d
Sanju-Reddy <reddysanju9898@gmail.com>
2024-05-01 21:03:27 +0530

    Functions

--- common.sh
+++ common.sh
@@ -2,10 +2,9 @@
 
 #set -e 
 
-handle_error(){
-      echo "Error occured at line number: $1, error command: $2"
-}
-
+#handle_error(){
+#     echo "Error occured at line number: $1, error command: $2"
+#}
 #trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 
