fac41c15812fbd8e84ef2874a945689d32dfbd9b
Sanju-Reddy <reddysanju9898@gmail.com>
2024-05-01 17:05:44 +0530

    Functions

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,7 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-serffver -y &>>$LOGFILE
+dnf install mysql-server -y &>>$LOGFILE
 
 systemctl enable mysqld &>>$LOGFILE
 
